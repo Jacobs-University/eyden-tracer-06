d3c30b4e778e9fea3eedd590f6b502128116cde7
Sergey Kosov <sergey.kosov@project-10.de>
2020-11-30 17:24:06 +0100

    Video generation framework

--- src/main.cpp
+++ src/main.cpp
@@ -24,6 +24,9 @@ Mat RenderFrame(void)
 {
 	// Camera resolution
 	const Size resolution(768, 480);
+
+	// number of sides of the spheres
+	const size_t nSides = 24;
 	
 	// Background color
 	const Vec3f bgColor = RGB(0, 0, 0);
@@ -63,8 +66,8 @@ Mat RenderFrame(void)
 	auto sun = std::make_shared<CLightOmni>(Vec3f::all(3e10), Vec3f(0, 0, 0));
 
 	// Geometry
-	auto earth = CSolidSphere(pShaderEarth, Vec3f(150000, 0, 0), 6.371f);
-	auto moon = CSolidSphere(pShaderMoon, Vec3f(150000, 0, -384), 1.737f);
+	auto earth = CSolidSphere(pShaderEarth, Vec3f(150000, 0, 0), 6.371f, nSides);
+	auto moon = CSolidSphere(pShaderMoon, Vec3f(150000, 0, -384), 1.737f, nSides);
 
 	// Add everything to the scene
 	scene.add(sun);
@@ -72,24 +75,43 @@ Mat RenderFrame(void)
 	scene.add(moon);
 
 	// Build BSPTree
-	scene.buildAccelStructure(20, 3);
+	scene.buildAccelStructure(0, 3);
 
 	scene.setActiveCamera(1);
 	Mat img(resolution, CV_32FC3);								// image array
+	Mat frame_img;
 	
-	img.setTo(0);
-	parallel_for_(Range(0, img.rows), [&](const Range& range) {
-		Ray ray;												// primary ray
-		for (int y = range.start; y < range.end; y++) {
-			Vec3f* pImg = img.ptr<Vec3f>(y);					// fast processing via pointers
-			for (int x = 0; x < img.cols; x++) {
-				scene.getActiveCamera()->InitRay(ray, x, y, Vec2f::all(0.5f));	// initialize ray
-				pImg[x] = scene.RayTrace(ray);
-			} // x
-		} // y
-	});
-	img.convertTo(img, CV_8UC3, 255);
-	return img;
+	const size_t nFrames = 1;										// 180 frames - 6 seconds of video
+	VideoWriter videoWriter;
+	if (nFrames) {
+		auto codec = VideoWriter::fourcc('M', 'J', 'P', 'G');		// Native windows codec
+		//auto codec = VideoWriter::fourcc('H', '2', '6', '4');		// Try it on MacOS
+		videoWriter.open("video.avi", codec, 30, resolution);
+		if (!videoWriter.isOpened()) printf("ERROR: Can't open vide file for writing\n");
+	}
+
+
+	for (size_t frame = 0; frame < nFrames; frame++) {
+		img.setTo(0);
+		parallel_for_(Range(0, img.rows), [&](const Range& range) {
+			Ray ray;												// primary ray
+			for (int y = range.start; y < range.end; y++) {
+				Vec3f* pImg = img.ptr<Vec3f>(y);					// fast processing via pointers
+				for (int x = 0; x < img.cols; x++) {
+					scene.getActiveCamera()->InitRay(ray, x, y, Vec2f::all(0.5f));	// initialize ray
+					pImg[x] = scene.RayTrace(ray);
+				} // x
+			} // y
+			});
+		img.convertTo(frame_img, CV_8UC3, 255);
+		if (nFrames > 1) {
+			videoWriter << frame_img;
+			imshow("frame", frame_img);
+			printf("Frame %zu / %zu\n", frame, nFrames);
+			waitKey(5);
+		}
+	}
+	return frame_img;
 }
 
 int main(int argc, char* argv[])
